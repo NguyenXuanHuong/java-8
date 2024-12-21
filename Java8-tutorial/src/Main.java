4454201f5dd7360e0c75c587adb85b6c8402eb9a
xuanhuong <xuanhuong@gmail.com>
2024-12-21 09:24:43 +0900

    add functional interface, impl class and lambda fucntion

--- Java8-tutorial/src/Main.java
+++ Java8-tutorial/src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //インターフェースのインスタンスを作成する普通の方法です。
+        //インターフェースを実装するクラスを作成する
+        SampleFunctionalInterface sampleFunctionalInterface = new SampleFunctionalInterfaceImplementation();
+        System.out.println("output from class: " + sampleFunctionalInterface.singleFunction(1));
+        //機能式インターフェースのインスタンスを作成するためにラムダ式を使用する。
+        SampleFunctionalInterface sampleLambdaFunction = (a) -> {return a + 1;};
+        System.out.println("output from lambda: " + sampleLambdaFunction.singleFunction(1));
+
     }
 }
---
+++ Java8-tutorial/src/SampleFunctionalInterface.java
@@ -0,0 +1,5 @@
+// 機能式インターフェースの例
+public interface SampleFunctionalInterface {
+    //単一の関数を持つ
+    int singleFunction(int input);
+}
