0fa480bc7f3db99bfb0e08a4512e69aa29d20972
xuanhuong <xuanhuong@gmail.com>
2024-12-22 19:49:53 +0900

    add the third consumer interface to demonstrate the andThen method

--- Java8-tutorial/src/Main.java
+++ Java8-tutorial/src/Main.java
@@ -2,7 +2,6 @@ import java.util.function.Consumer;
 
 public class Main {
     public static void main(String[] args) {
-        // Consumer to increase the input
         Consumer<Integer> increase = input ->
         {
             System.out.println("original input value is: " + input);
@@ -14,18 +13,26 @@ public class Main {
         int inputValue = 1;
         increase.accept(inputValue);
 
-        // Consumer to multiply the input
-        Consumer<Integer> multiply = input -> {
+        Consumer<Integer> multiplyBy2 = input -> {
             System.out.println("original input value is: " + input);
             input = input * 2;
-            System.out.println("multiply lambda: accept method was executed");
+            System.out.println("multiplyBy2 lambda: accept method was executed");
             System.out.println("updated input value is: " + input);
             System.out.println("---------------end---------------");
         };
+
+        Consumer<Integer> multiplyBy3 = input -> {
+            System.out.println("original input value is: " + input);
+            input = input * 3;
+            System.out.println("multiplyBy3 lambda: accept method was executed");
+            System.out.println("updated input value is: " + input);
+            System.out.println("---------------end---------------");
+        };
+
         // using addThen()
-        Consumer<Integer> theNewSequenceConsumer = increase.andThen(multiply);
+        Consumer<Integer> theNewSequenceConsumer = increase.andThen(multiplyBy2).andThen(multiplyBy3);
         System.out.println();
-        System.out.println("------start executing the sequence of two lambda--------");
+        System.out.println("------start executing the sequence of three lambda--------");
         theNewSequenceConsumer.accept(inputValue);
     }
 }
