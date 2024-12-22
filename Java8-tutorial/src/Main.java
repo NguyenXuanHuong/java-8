687a724c2fab2fde2aafdd77ee9be880a6c781ac
xuanhuong <xuanhuong@gmail.com>
2024-12-22 09:09:45 +0900

    add sample functional interface

--- Java8-tutorial/src/Main.java
+++ Java8-tutorial/src/Main.java
@@ -1,15 +1,7 @@
+import java.util.function.Consumer;
+
 public class Main {
     public static void main(String[] args) {
-        // 実質的にfinalな変数
-        int outside = 1;
-        SingleParam singleParam = (input) -> {
-            //ok
-            input = outside + 10;
-//            outside++; できない
-            System.out.println("value of input: " + input);
-            return input;
-        };
-        singleParam.increaseOutsideVariable(10);
-        System.out.println(outside);
+        System.out.println("hello");
     }
 }
--- Java8-tutorial/src/SingleParam.java
+++
@@ -1,3 +0,0 @@
-public interface SingleParam {
-    int increaseOutsideVariable(int input);
-}
