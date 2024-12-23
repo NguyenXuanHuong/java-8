203c4535b2ada09bf12a1ccdbe82ff3338209d02
xuanhuong <xuanhuong@gmail.com>
2024-12-23 23:21:47 +0900

    replace the or method logic by a more easy to understand logic

--- Java8-tutorial/src/PredicateOrMethod.java
+++ Java8-tutorial/src/PredicateOrMethod.java
@@ -2,13 +2,20 @@ import java.util.function.Predicate;
 
 public class PredicateOrMethod {
     public static void main(String[] args) {
-        Predicate<String> containsLetterA = p -> p.contains("A");
-        Predicate<String> hasLengthOf10 = p -> p.length() == 10;
-        String containsA = "And";
-        String notContainsA = "Then";
-        boolean trueValue = hasLengthOf10.or(containsLetterA).test(containsA);
-        boolean falseValue = hasLengthOf10.or(containsLetterA).test(notContainsA);
+        Predicate<Integer> greaterThan1 = input -> input > 1;
+        Predicate<Integer> greaterThan2 = input -> input > 2;
+        Predicate<Integer> greaterThan3 = input -> input > 3;
+
+        int trueInput = 2;
+        int falseInput = 0;
+        /*
+        greaterThan1.test(input) || greaterThan2.test(containsLetterA) || greaterThan3.test(input)
+        * */
+        Predicate<Integer> sequencePredicate = greaterThan1.or(greaterThan2).or(greaterThan3);
+        boolean trueValue = sequencePredicate.test(trueInput);
         System.out.println(trueValue);
+
+        boolean falseValue = sequencePredicate.test(falseInput);
         System.out.println(falseValue);
     }
 }
