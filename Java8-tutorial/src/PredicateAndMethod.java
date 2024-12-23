464614397fc21776316237ea204d3e830793e55d
xuanhuong <xuanhuong@gmail.com>
2024-12-23 22:36:35 +0900

    add detail explanation for and method

--- Java8-tutorial/src/PredicateAndMethod.java
+++ Java8-tutorial/src/PredicateAndMethod.java
@@ -8,6 +8,9 @@ public class PredicateAndMethod {
 
         String stringContainAAndLengthIs3 = "Abc";
 
+        /*
+        containsLetterA.test(containsLetterA) && hasLengthOf3.test(containsLetterA)
+        * */
         Predicate<String> stringContainAAndLengthIs3Predicate = containsLetterA.and(hasLengthOf3);
         boolean trueValue = stringContainAAndLengthIs3Predicate.test(stringContainAAndLengthIs3);
         System.out.println(trueValue);
