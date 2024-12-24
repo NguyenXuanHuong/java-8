8cf2d39a50b8edbc784d296c87205ae56f166f39
xuanhuong <xuanhuong@gmail.com>
2024-12-24 21:24:36 +0900

    rename the negate lambda and fix the isEqual logic

--- Java8-tutorial/src/PredicateIsEqualMethod.java
+++ Java8-tutorial/src/PredicateIsEqualMethod.java
@@ -2,8 +2,8 @@ import java.util.function.Predicate;
 
 public class PredicateIsEqualMethod {
     public static void main(String[] args) {
-        Predicate<Integer> isEqualTo3 = (input) -> input == 3;
-        boolean trueValue = isEqualTo3.test(3);
-        System.out.println(trueValue);
+       Predicate<Integer> isEqualTo3Predicate = Predicate.isEqual(3);
+       System.out.println(isEqualTo3Predicate.test(3));
+       System.out.println(isEqualTo3Predicate.test(4));
     }
 }
--- Java8-tutorial/src/PredicateNegateMethod.java
+++ Java8-tutorial/src/PredicateNegateMethod.java
@@ -3,9 +3,10 @@ import java.util.function.Predicate;
 public class PredicateNegateMethod {
     public static void main(String[] args) {
         Predicate<Integer> greaterThan2 = input -> input > 2;
+        Predicate<Integer> lessThan2 = input -> input < 2;
         int input = 1;
-        Predicate<Integer> lessThan2 = greaterThan2.negate();
-        boolean trueValue = lessThan2.test(input);
+        Predicate<Integer> greaterThan2Negate = greaterThan2.negate();
+        boolean trueValue = greaterThan2Negate.test(input);
         System.out.println(trueValue);
     }
 }
