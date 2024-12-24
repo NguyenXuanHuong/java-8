9b61d813f8428f2b692353f695bbe6652c48c7ec
xuanhuong <xuanhuong@gmail.com>
2024-12-24 22:00:30 +0900

    add more explanation for not function

--- Java8-tutorial/src/PredicateNotMethod.java
+++ Java8-tutorial/src/PredicateNotMethod.java
@@ -3,6 +3,7 @@ import java.util.function.Predicate;
 public class PredicateNotMethod {
     public static void main(String[] args) {
         Predicate<Integer> isEqual3 = input -> input == 3;
+//         Predicate<Integer> notEqual3 = isEqual3.negate();
         Predicate<Integer> notEqual3 = Predicate.not(isEqual3);
         System.out.println(notEqual3.test(3));
     }
