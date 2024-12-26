331d41ce1b12d27fb452fcc5472c95849ca70388
xuanhuong <xuanhuong@gmail.com>
2024-12-27 07:30:11 +0900

    add missing class

---
+++ Java8-tutorial/src/staticMethodReference/DivideMethodClass.java
@@ -0,0 +1,7 @@
+package staticMethodReference;
+
+public class DivideMethodClass {
+    public static float staticDivideFunction(int input1, int input2){
+        return (float) input1 / input2;
+    }
+}
