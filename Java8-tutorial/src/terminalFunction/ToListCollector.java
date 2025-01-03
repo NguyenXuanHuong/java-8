2b875be2025b312c71fbca91fa92f4731d966af3
xuanhuong <xuanhuong@gmail.com>
2025-01-03 23:19:09 +0900

    add main method to toList collector

--- Java8-tutorial/src/terminalFunction/ToListCollector.java
+++ Java8-tutorial/src/terminalFunction/ToListCollector.java
@@ -5,7 +5,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class ToListCollector {
-    List<Integer> list = List.of(1,2,3);
-    Stream<Integer> listPlus1Stream = list.stream().map(input -> input + 1);
-    List<Integer> listPlus1 = listPlus1Stream.collect(Collectors.toList());
+    public static void main(String[] args) {
+        List<Integer> list = List.of(1,2,3);
+        Stream<Integer> listPlus1Stream = list.stream().map(input -> input + 1);
+        List<Integer> listPlus1 = listPlus1Stream.collect(Collectors.toList());
+        System.out.println(listPlus1);
+    }
 }
