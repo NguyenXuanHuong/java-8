1cdfc94c7eb48e45b96437c8c44971519145465c
xuanhuong <xuanhuong@gmail.com>
2025-01-04 17:54:09 +0900

    replace the old logic by the Optional logic

---
+++ Java8-tutorial/src/ClassLevel1.java
@@ -0,0 +1,15 @@
+public class ClassLevel1 {
+    private ClassLevel2 classLevel2;
+
+    public ClassLevel1(ClassLevel2 classLevel2) {
+        this.classLevel2 = classLevel2;
+    }
+
+    public ClassLevel2 getClassLevel2() {
+        return classLevel2;
+    }
+
+    public void setClassLevel2(ClassLevel2 classLevel2) {
+        this.classLevel2 = classLevel2;
+    }
+}
