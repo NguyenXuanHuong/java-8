219485d6d138adf8ebe0263bf596dcbb4ab8e4b7
xuanhuong <xuanhuong@gmail.com>
2025-01-06 20:34:58 +0900

    rename method only keep the compare method

--- Java8-tutorial/src/LocalDateCompareMethod.java
+++ Java8-tutorial/src/LocalDateCompareMethod.java
@@ -1,15 +1,13 @@
 import java.time.LocalDate;
 
-public class LocalDateAdditionalMethod {
+public class LocalDateCompareMethod {
     public static void main(String[] args) {
         LocalDate localDate = LocalDate.now();
-        System.out.println("the original value: " + localDate);
-        System.out.println("this year is a leap year: " + localDate.isLeapYear());
 
-        LocalDate localDate1 = LocalDate.of(2025, 1, 5);
+        LocalDate localDate1 = LocalDate.of(2025, 1, 6);
         System.out.println("isEqual : " + localDate1.isEqual(localDate1));
 
-        LocalDate localDate2 = LocalDate.of(2025, 1, 6);
+        LocalDate localDate2 = LocalDate.of(2025, 1, 7);
         System.out.println("isBefore : " + localDate1.isBefore(localDate));
         System.out.println("isAfter : " + localDate1.isAfter(localDate2));
 
