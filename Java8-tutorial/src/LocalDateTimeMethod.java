c289980f9e50058ef5b5daa8dddc190727972157
xuanhuong <xuanhuong@gmail.com>
2025-01-07 21:33:33 +0900

    add method to change localdatetime to localdate and localtime

--- Java8-tutorial/src/LocalDateTimeMethod.java
+++ Java8-tutorial/src/LocalDateTimeMethod.java
@@ -6,7 +6,7 @@ import java.time.temporal.TemporalAdjusters;
 
 public class LocalDateTimeMethod {
     public static void main(String[] args) {
-        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now().minusDays(1);
         System.out.println("localDateTime :" + localDateTime);
         LocalDateTime localDateTime1  = LocalDateTime.of(2018, 01,01,12,10,00);
         System.out.println("localDateTime1 : " + localDateTime1);
@@ -26,6 +26,9 @@ public class LocalDateTimeMethod {
         System.out.println("withMonth : " + localDateTime.withMonth(2));
         System.out.println("with : " + localDateTime.with(ChronoField.MONTH_OF_YEAR,3));
         System.out.println("firstDayOfNextMonth : " + localDateTime.with(TemporalAdjusters.firstDayOfNextMonth()));
+
+        System.out.println("toLocalDate : " + localDateTime.toLocalDate());
+        System.out.println("toLocalTime : " + localDateTime.toLocalTime());
 
     }
 }
